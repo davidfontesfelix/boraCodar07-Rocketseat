40ef984d43a8e31392e9eeee513d0b59fc91bab6
haxizi <haxihx0@gmail.com>
2023-02-19 12:33:51 -0300

    refactor: alguns ajustes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,12 +6,12 @@ export default function Card(props) {
     return (
 
     <div className="card">
-        <img src={props.img} />
+        <img src={props.img} alt="imagem do evento" />
         <div className="infos">
             <h2>{props.nome}</h2>
             <p>{props.texto}</p>
             <div className="locate">
-                <h3> <img src={location}/> {props.cidade} - {props.estado}</h3>
+                <h3> <img src={location} alt="icone de localização" /> {props.cidade} - {props.estado}</h3>
             </div>
         </div>
     </div>
